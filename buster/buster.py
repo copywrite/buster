05a0947541cb9a5ca76613bb6ba3adbe16c9e2a6
地里 <pengdi.pd@alibaba-inc.com>
2016-01-18 19:09:12 +0800

    drop querystring

--- buster/buster.py
+++ buster/buster.py
@@ -8,6 +8,8 @@ Usage:
   buster.py add-domain <domain-name> [--dir=<path>]
   buster.py (-h | --help)
   buster.py --version
+  Notes:
+    1. generate function did not drop the static file path which contains version parameter, fixed it 
 
 Options:
   -h --help                 Show this screen.
@@ -15,6 +17,7 @@ Options:
   --dir=<path>              Absolute path of directory to store static pages.
   --domain=<local-address>  Address of local ghost installation [default: localhost:2368].
   --gh-repo=<repo-url>      URL of your gh-pages repository.
+
 """
 
 import os
@@ -28,9 +31,12 @@ from docopt import docopt
 from time import gmtime, strftime
 from git import Repo
 from pyquery import PyQuery
-
+from urlparse import urlparse
 
 def main():
+    is_windows = os.name == 'nt'
+    query_string_separator = '@' if is_windows else '#'
+
     arguments = docopt(__doc__, version='0.1.3')
     if arguments['--dir'] is not None:
         static_path = arguments['--dir']
@@ -40,53 +46,111 @@ def main():
     if arguments['generate']:
         command = ("wget "
                    "--recursive "             # follow links to download entire site
-                   "--convert-links "         # make links relative
+                   "{2} "         # make links relative
                    "--page-requisites "       # grab everything: css / inlined images
                    "--no-parent "             # don't go to parent level
                    "--directory-prefix {1} "  # download contents to static/ folder
                    "--no-host-directories "   # don't create domain named folder
-                   "--restrict-file-name=unix "  # don't escape query string
-                   "{0}").format(arguments['--domain'], static_path)
-        os.system(command)
+                   "--restrict-file-name={3} "  # don't escape query string
+                   "{0}").format(arguments['--domain'], static_path, '' if is_windows else '--convert-links', 'windows' if is_windows else 'unix')
+        result = os.system(command)
+
+        if result > 0:
+            raise IOError('Your ghost server is dead')
 
         # remove query string since Ghost 0.4
-        file_regex = re.compile(r'.*?(\?.*)')
+        file_regex = re.compile(r'.*?(' + query_string_separator + '.*)')
+        html_regex = re.compile(r".*?(\.html)")
+
         for root, dirs, filenames in os.walk(static_path):
             for filename in filenames:
+                if is_windows and html_regex.match(filename):
+                    path = ("{0}").format(os.path.join(root, filename).replace("\\", "/"))
+                    with open(path, "r+") as f:
+                        file_contents = f.read()
+                        file_contents = file_contents.replace(arguments['--domain'], "")
+                        file_contents = file_contents.replace("%hurl", arguments['--domain'])
+                        f.seek(0)
+                        f.write(file_contents)
+                        f.close()
+
+
                 if file_regex.match(filename):
-                    newname = re.sub(r'\?.*', '', filename)
-                    print "Rename", filename, "=>", newname
-                    os.rename(os.path.join(root, filename), os.path.join(root, newname))
+                    newname = re.sub(query_string_separator + r'.*', '', filename)
+                    newpath = os.path.join(root, newname)
+                    try:
+                        os.remove(newpath)
+                    except OSError:
+                        pass
+
+                    os.rename(os.path.join(root, filename), newpath)
+                else:
+                    print '// Drop queryString in filename'
+                    print 'from: ', filename
+                    newname = urlparse(filename).path
+                    print 'to: ', newname
+
+                    if newname != filename:
+                        newpath = os.path.join(root, newname)
+                        try:
+                            os.remove(newpath)
+                        except OSError:
+                            pass
+
+                        os.rename(os.path.join(root, filename), newpath)
 
         # remove superfluous "index.html" from relative hyperlinks found in text
         abs_url_regex = re.compile(r'^(?:[a-z]+:)?//', flags=re.IGNORECASE)
         def fixLinks(text, parser):
             d = PyQuery(bytes(bytearray(text, encoding='utf-8')), parser=parser)
-            for element in d('a'):
+            for element in d('a, link'):
                 e = PyQuery(element)
                 href = e.attr('href')
+
+                if href is None:
+                    continue
+
+                print '// Drop queryString in included src'
+                print 'from: ', href
+                result = urlparse(href)
+
+                if result.scheme == 'https':
+                    href = href
+                elif result.scheme == '':
+                    href = result.path + (('#' + result.fragment) if result.fragment != '' else '')
+                print 'to: ', href
+  
+                new_href = re.sub(r'(rss/index\.html)|(rss/?)$', 'rss/index.rss', href)
                 if not abs_url_regex.search(href):
-                    new_href = re.sub(r'rss/index\.html$', 'rss/index.rss', href)
                     new_href = re.sub(r'/index\.html$', '/', new_href)
+
+                if href != new_href:
                     e.attr('href', new_href)
                     print "\t", href, "=>", new_href
+
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
 
         # fix links in all html files
         for root, dirs, filenames in os.walk(static_path):
-            for filename in fnmatch.filter(filenames, "*.html"):
+            for filename in fnmatch.filter(filenames, '*.html'):
                 filepath = os.path.join(root, filename)
                 parser = 'html'
-                if root.endswith("/rss"):  # rename rss index.html to index.rss
+                if root.endswith(os.path.sep + 'rss'):  # rename rss index.html to index.rss
                     parser = 'xml'
-                    newfilepath = os.path.join(root, os.path.splitext(filename)[0] + ".rss")
+                    newfilepath = os.path.join(root, os.path.splitext(filename)[0] + '.rss')
+                    
+                    try:
+                        os.remove(newfilepath)
+                    except OSError:
+                        pass
+
                     os.rename(filepath, newfilepath)
                     filepath = newfilepath
                 with open(filepath) as f:
                     filetext = f.read().decode('utf8')
-                print "fixing links in ", filepath
+                print 'fixing links in ', filepath
                 newtext = fixLinks(filetext, parser)
                 with open(filepath, 'w') as f:
                     f.write(newtext)
